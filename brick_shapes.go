7d9378ea087f25c5631f3c4a45298fead63ff939
Nick Dunn <nicholas.c.dunn@gmail.com>
2014-02-22 10:56:19 -0800

    Adding an interface for brick so we can sort out some of this casting garbage

--- brick_shapes.go
+++ brick_shapes.go
@@ -1,7 +1,16 @@
 package BrickMosaic
 
 // Brick represents a prototypical piece, not bound to any specific orientation or color.
-type Brick struct {
+type Brick interface {
+  Name() string
+  Id() string
+  Width() int
+  Length() int
+  Height() int
+}
+
+// brick represents a prototypical piece, not bound to any specific orientation or color.
+type brick struct {
 	// name is the human readable name for this brick.
 	name string
 	// id is the the LDRAW identifier for this Brick.
@@ -14,9 +23,29 @@ type Brick struct {
 	height int
 }
 
+func (b brick) Name() string {
+  return b.name
+}
+
+func (b brick) Id() string {
+  return b.id
+}
+
+func (b brick) Width() int {
+  return b.width
+}
+
+func (b brick) Length() int {
+  return b.length
+}
+
+func (b brick) Height() int {
+  return b.height
+}
+
 var (
 	// OneByFour represents a 1 x 4 brick. See http://lego.wikia.com/wiki/Part_3010.
-	OneByFour = Brick{
+	OneByFour = brick{
 		name:   "1x4 brick",
 		id:     "3010",
 		width:  1,
@@ -24,7 +53,7 @@ var (
 		height: 3,
 	}
 	// OneByThree represents a 1 x 3 brick. See http://lego.wikia.com/wiki/Part_3622.
-	OneByThree = Brick{
+	OneByThree = brick{
 		name:   "1x3 brick",
 		id:     "3622",
 		width:  1,
@@ -32,7 +61,7 @@ var (
 		height: 3,
 	}
 	// OneByTwo represents a 1 x 2 brick. See http://lego.wikia.com/wiki/Part_3004.
-	OneByTwo = Brick{
+	OneByTwo = brick{
 		name:   "1x2 brick",
 		id:     "3004",
 		width:  1,
@@ -40,7 +69,7 @@ var (
 		height: 3,
 	}
 	// OneByOne represents a 1 x 1 brick. See http://lego.wikia.com/wiki/Part_3005.
-	OneByOne = Brick{
+	OneByOne = brick{
 		name:   "1x1 brick",
 		id:     "3005",
 		width:  1,
@@ -48,7 +77,7 @@ var (
 		height: 3,
 	}
 	// TwoByFour represents a 2 x 4 brick. See http://lego.wikia.com/wiki/Part_3001.
-	TwoByFour = Brick{
+	TwoByFour = brick{
 		name:   "2x4 brick",
 		id:     "3001",
 		width:  2,
@@ -56,7 +85,7 @@ var (
 		height: 3,
 	}
 	// TwoByThree represents a 2 x 3 brick. See http://lego.wikia.com/wiki/Part_3002.
-	TwoByThree = Brick{
+	TwoByThree = brick{
 		name:   "2x3 brick",
 		id:     "3002",
 		width:  2,
@@ -64,7 +93,7 @@ var (
 		height: 3,
 	}
 	// TwoByTwo represents a 2 x 2 brick. See http://lego.wikia.com/wiki/Part_3003.
-	TwoByTwo = Brick{
+	TwoByTwo = brick{
 		name:   "2x2 brick",
 		id:     "3003",
 		width:  2,
@@ -75,7 +104,7 @@ var (
 	// Plates
 
 	// OneByPlate represents a 1 x 1 plate. See http://lego.wikia.com/wiki/Part_3024.
-	OneByOnePlate = Brick{
+	OneByOnePlate = brick{
 		name:   "1x1 plate",
 		id:     "3024",
 		width:  1,
@@ -83,7 +112,7 @@ var (
 		height: 1,
 	}
 	// OneByTwoPlate represents a 1 x 2 plate. See http://lego.wikia.com/wiki/Part_3023.
-	OneByTwoPlate = Brick{
+	OneByTwoPlate = brick{
 		name:   "1x2 plate",
 		id:     "3023",
 		width:  1,
@@ -91,7 +120,7 @@ var (
 		height: 1,
 	}
 	// OneByThreePlate represents a 1 x 3 plate. See http://lego.wikia.com/wiki/Part_3623.
-	OneByThreePlate = Brick{
+	OneByThreePlate = brick{
 		name:   "1x3 plate",
 		id:     "3623",
 		width:  1,
@@ -99,7 +128,7 @@ var (
 		height: 1,
 	}
 	// OneByFourPlate represents a 1 x 4 plate. See http://lego.wikia.com/wiki/Part_3710.
-	OneByFourPlate = Brick{
+	OneByFourPlate = brick{
 		name:   "1x4 plate",
 		id:     "3710",
 		width:  1,
@@ -107,7 +136,7 @@ var (
 		height: 1,
 	}
 	// OneBySixPlate represents a 1 x 6 plate. See http://lego.wikia.com/wiki/Part_3666.
-	OneBySixPlate = Brick{
+	OneBySixPlate = brick{
 		name:   "1x6 plate",
 		id:     "3666",
 		width:  1,
@@ -115,7 +144,7 @@ var (
 		height: 1,
 	}
 	// OneByEightPlate represents a 1 x 8 plate. See http://brickowl.com/catalog/lego-plate-1-x-8-3460.
-	OneByEightPlate = Brick{
+	OneByEightPlate = brick{
 		name:   "1x8 plate",
 		id:     "3460",
 		width:  1,
@@ -123,7 +152,7 @@ var (
 		height: 1,
 	}
 	// OneByTenPlate represents a 1 x 10 plate. See http://brickowl.com/catalog/lego-plate-1-x-10-4477.
-	OneByTenPlate = Brick{
+	OneByTenPlate = brick{
 		name:   "1x10 plate",
 		id:     "4477",
 		width:  1,
@@ -183,7 +212,7 @@ func (r MosaicPiece) Extent() []Location {
 
 func StudsOutPiece(piece Brick) MosaicPiece {
 	// Studs up, so rows = width, cols = length
-	r := RectPiece{piece.width, piece.length}
+	r := RectPiece{piece.Width(), piece.Length()}
 	return MosaicPiece{
 		piece,
 		r,
@@ -193,7 +222,7 @@ func StudsOutPiece(piece Brick) MosaicPiece {
 
 func StudsTopPiece(piece Brick) MosaicPiece {
 	// Studs to the top on side, so rows = height, cols = length
-	r := RectPiece{piece.height, piece.length}
+	r := RectPiece{piece.Height(), piece.Length()}
 	return MosaicPiece{
 		piece,
 		r,
@@ -203,7 +232,7 @@ func StudsTopPiece(piece Brick) MosaicPiece {
 
 func StudsRightPiece(piece Brick) MosaicPiece {
 	// Studs to the right on its side, so rows = length, cols = height
-	r := RectPiece{piece.length, piece.height}
+	r := RectPiece{piece.Length(), piece.Height()}
 	return MosaicPiece{
 		piece,
 		r,
